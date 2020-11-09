85e567ec7f428078e7a18fa2b54db5faf4c18823
Gregory Casamento <greg.casamento@gmail.com>
2020-11-09 17:08:09 -0500

    Initial

--- RigilTest/ViewController.m
+++ RigilTest/ViewController.m
@@ -9,14 +9,40 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)updateButton:(id)sender
+{
+    [self.tableView reloadData];
+}
+
+// Table data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.nameTextField.text length];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"StandardCell"];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] init];
+    }
+    NSString *ch = [self.nameTextField.text substringWithRange: NSMakeRange(indexPath.row,1)];
+    [cell setText: ch];
+    return cell;
+}
 
 @end
